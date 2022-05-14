7425857e2889252d752b27de45de629a5c773c70
CJM1994 <connorjohnmullin@gmail.com>
2022-05-14 15:09:42 -0700

    make input field generic, refactor

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,11 +1,24 @@
-import { FC } from 'react'
+import { FormControl, FormErrorMessage, FormLabel, Input } from '@chakra-ui/react'
+import { useField } from 'formik';
+import { FC, InputHTMLAttributes } from 'react'
 
-interface InputFieldProps {
-
+type InputFieldProps = InputHTMLAttributes<HTMLInputElement> & {
+  name: string,
+  label: string,
+  placeholder: string,
 }
 
-const InputField: FC<InputFieldProps> = () => {
-  return (<></>)
+const InputField: FC<InputFieldProps> = (props) => {
+
+  const [field, { error }] = useField(props);
+
+  return (
+    <FormControl isInvalid={!!error}>
+      <FormLabel htmlFor={field.name}>{props.label}</FormLabel>
+      <Input {...field} id={field.name} placeholder={props.placeholder} />
+      {error && <FormErrorMessage>{error}</FormErrorMessage>}
+    </FormControl>
+  )
 }
 
 export default InputField
--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -16,12 +16,6 @@ const Register: FC<registerProps> = () => {
       {({ values, handleChange }) => (
         <Form>
 
-          <FormControl>
-            <FormLabel htmlFor='username'>Username</FormLabel>
-            <Input value={values.username} onChange={handleChange} id='username' placeholder='username' />
-            {/* <FormErrorMessage>{form.errors.name}</FormErrorMessage> */}
-          </FormControl>
-
         </Form>
       )}
     </Formik>
