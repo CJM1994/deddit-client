3ed05ea93d7a0993f912ca7dd7de3d957f8c15b6
CJM1994 <connorjohnmullin@gmail.com>
2022-05-16 00:52:26 -0700

    add error handling and route to home from register page

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -4,18 +4,28 @@ import InputField from '../components/InputField';
 import { Button } from '@chakra-ui/react';
 import Wrapper from '../components/Wrapper';
 import { useRegisterMutation } from '../generated/graphql';
+import { errorMap } from '../utils/errorMap';
+import { useRouter } from 'next/router';
 
 interface registerProps { }
 
 const Register: FC<registerProps> = () => {
 
   const [registerUserResult, registerUser] = useRegisterMutation()
+  const router = useRouter()
 
   return (
     <Wrapper variant='small'>
       <Formik
         initialValues={{ username: '', password: '' }}
-        onSubmit={values => { return registerUser(values) }}
+        onSubmit={async (values, { setErrors }) => {
+          const response = await registerUser(values);
+          if (response.data?.register.errors) {
+            setErrors(errorMap(response.data.register.errors))
+          } else if (response.data?.register.user) {
+            router.push('/');
+          }
+        }}
       >
         {({ isSubmitting }) => (
           <Form>
