31fa2a9d27c7e53ac8d441ce85d945d9dc7c6ec8
CJM1994 <connorjohnmullin@gmail.com>
2022-05-17 00:22:51 -0700

    add user check logic to navbar, need to set caching manually so login/register does not display when first logged in

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,23 +1,37 @@
 import { Box, Flex, Link } from '@chakra-ui/react';
-import { FC } from 'react';
+import { FC, useEffect } from 'react';
 import NextLink from 'next/link';
+import { useMeQuery } from '../generated/graphql';
 
 interface NavbarProps { }
 
 const Navbar: FC<NavbarProps> = () => {
+
+  const [{ data, error }] = useMeQuery();
+  let userLinks = null;
+  console.log('data', data)
+  console.log('error', error)
+
+  if (!data?.me) {
+    userLinks =
+      <>
+        <Box marginLeft='auto'>
+          <NextLink href='/login'>
+            <Link marginRight={2}>Login</Link>
+          </NextLink>
+          <NextLink href='/register'>
+            <Link>Register</Link>
+          </NextLink>
+        </Box>
+      </>
+  }
+
   return (
     <Flex
       background='tomato'
       padding={4}
     >
-      <Box marginLeft='auto'>
-        <NextLink href='/login'>
-          <Link marginRight={2}>Login</Link>
-        </NextLink>
-        <NextLink href='/register'>
-          <Link>Register</Link>
-        </NextLink>
-      </Box>
+      {userLinks}
     </Flex>
   )
 }
--- src/generated/graphql.tsx
+++ src/generated/graphql.tsx
@@ -113,6 +113,11 @@ export type RegisterMutationVariables = Exact<{
 
 export type RegisterMutation = { __typename?: 'Mutation', register: { __typename?: 'UserResponse', user?: { __typename?: 'User', id: number, username: string, createdAt: string, updatedAt: string } | null, errors?: Array<{ __typename?: 'FieldError', field: string, message: string }> | null } };
 
+export type MeQueryVariables = Exact<{ [key: string]: never; }>;
+
+
+export type MeQuery = { __typename?: 'Query', me?: { __typename?: 'User', id: number, username: string, createdAt: string, updatedAt: string } | null };
+
 
 export const LoginDocument = gql`
     mutation Login($username: String!, $password: String!) {
@@ -153,4 +158,18 @@ export const RegisterDocument = gql`
 
 export function useRegisterMutation() {
   return Urql.useMutation<RegisterMutation, RegisterMutationVariables>(RegisterDocument);
+};
+export const MeDocument = gql`
+    query Me {
+  me {
+    id
+    username
+    createdAt
+    updatedAt
+  }
+}
+    `;
+
+export function useMeQuery(options?: Omit<Urql.UseQueryArgs<MeQueryVariables>, 'query'>) {
+  return Urql.useQuery<MeQuery>({ query: MeDocument, ...options });
 };
