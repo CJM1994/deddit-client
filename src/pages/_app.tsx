aa1477e3ce56950c45525f74886c1e23649bed4e
CJM1994 <connorjohnmullin@gmail.com>
2022-05-17 00:44:00 -0700

    install cacheExchange and add writeFragment function to update cache on login

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,9 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { ChakraProvider } from '@chakra-ui/react'
-import { createClient, Provider } from 'urql'
+import { createClient, dedupExchange, fetchExchange, Provider } from 'urql'
+import { cacheExchange } from '@urql/exchange-graphcache'
+import { MeDocument } from '../generated/graphql'
 
 // urql client for connecting to graphql
 const client = createClient({
@@ -9,7 +11,22 @@ const client = createClient({
   // fetchOptions required to return cookies to user
   fetchOptions: {
     credentials: 'include'
-  }
+  },
+  // using graphCache to normalize caching, needed for login to present properly
+  exchanges: [dedupExchange, fetchExchange, cacheExchange({
+    updates: {
+      Mutation: {
+        login: (result, args, cache, info) => {
+          cache.writeFragment(MeDocument, {
+            id: args.id,
+            username: args.username,
+            createdAt: args.createdAt,
+            updatedAt: args.updatedAt
+          })
+        }
+      }
+    }
+  })]
 })
 
 function MyApp({ Component, pageProps }: AppProps) {
